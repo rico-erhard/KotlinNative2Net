c52b438a390f6be781717b0547f0c8763f3f086e
rico-erhard <76253376+rico-erhard@users.noreply.github.com>
2022-06-04 21:57:51 +0200

    Refactor and extend the example.

--- Samples/Math/Program.cs
+++ Samples/Math/Program.cs
@@ -1,12 +1,13 @@
-﻿
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using KotlinNative2Net;
+using LanguageExt;
+using static System.Console;
 
 static string Hex(IntPtr ptr)
 => BitConverter.ToString(BitConverter.GetBytes(ptr.ToInt64()).Reverse().ToArray());
 
 static void PrintHexed(IntPtr ptr)
-=> Console.WriteLine(Hex(ptr));
+=> WriteLine(Hex(ptr));
 
 static T GetFunc<T>(IntPtr symbols, int offset)
 {
@@ -25,20 +26,33 @@ KStruct symbolsDecl = (KStruct)header.Childs.Find(x => x.Name == header.SymbolsT
 KStruct minusDecl = (KStruct)symbolsDecl.FindChild("Minus");
 KStruct plusDecl = (KStruct)symbolsDecl.FindChild("Plus");
 
+
+Func<KFunc, Option<int>> findOffset =
+f => symbolsDecl.FindOffset(f);
+
 KFunc plusTypeDecl = (KFunc)plusDecl.Funcs.Find(x => "_type" == x.Name);
-int plusTypeOffset = (int)symbolsDecl.FindOffset(plusTypeDecl);
+int plusTypeOffset = (int)findOffset(plusTypeDecl);
 
 KFunc plusCtorDecl = (KFunc)plusDecl.Funcs.Find(x => "Plus" == x.Name);
-int plusCtorOffset = (int)symbolsDecl.FindOffset(plusCtorDecl);
+int plusCtorOffset = (int)findOffset(plusCtorDecl);
+
+KFunc addDecl = (KFunc)plusDecl.Funcs.Find(x => "add" == x.Name);
+int addOffset = (int)findOffset(addDecl);
+
+KFunc minusCtorDecl = (KFunc)minusDecl.Funcs.Find(x => "Minus" == x.Name);
+int minusCtorOffset = (int)findOffset(minusCtorDecl);
+
+KFunc subtractMethodDecl = (KFunc)minusDecl.Funcs.Find(x => "subtract" == x.Name);
+int substractOffset = (int)findOffset(subtractMethodDecl);
 
 KFunc isInstanceDecl = (KFunc)symbolsDecl.Funcs.Find(x => "IsInstance" == x.Name);
-int isInstanceOffset = (int)symbolsDecl.FindOffset(isInstanceDecl);
+int isInstanceOffset = (int)findOffset(isInstanceDecl);
 
 KFunc createNullableUnitDecl = (KFunc)symbolsDecl.Funcs.Find(x => "createNullableUnit" == x.Name);
-int createNullableUnitOffset = (int)symbolsDecl.FindOffset(createNullableUnitDecl);
+int createNullableUnitOffset = (int)findOffset(createNullableUnitDecl);
 
 KFunc disposeStablePointerDecl = (KFunc)symbolsDecl.Funcs.Find(x => "DisposeStablePointer" == x.Name);
-int disposeStablePointerOffset = (int)symbolsDecl.FindOffset(createNullableUnitDecl);
+int disposeStablePointerOffset = (int)findOffset(createNullableUnitDecl);
 
 IntPtr mathLib = NativeLibrary.Load(sharedLibPath);
 IntPtr symbolsFuncAddr = NativeLibrary.GetExport(mathLib, header.SymbolsFunc);
@@ -47,46 +61,47 @@ IntPtr symbols = symbolsFunc();
 
 int step = IntPtr.Size;
 
-IntPtr plusTypeAddr = Marshal.ReadIntPtr(symbols + step * plusTypeOffset);
-Void_IntPtr plusType = Marshal.GetDelegateForFunctionPointer<Void_IntPtr>(plusTypeAddr);
+Void_IntPtr plusType = GetFunc<Void_IntPtr>(symbols, plusTypeOffset);
+PlusCtor plusCtor = GetFunc<PlusCtor>(symbols, plusCtorOffset);
+MinusCtor minusCtor = GetFunc<MinusCtor>(symbols, minusCtorOffset);
+Ptr_Int addMethod = GetFunc<Ptr_Int>(symbols, addOffset);
+Ptr_Int subtractMethod = GetFunc<Ptr_Int>(symbols, substractOffset);
 
-IntPtr plusAddr = Marshal.ReadIntPtr(symbols + step * plusCtorOffset);
-PlusCtor plusCtor = Marshal.GetDelegateForFunctionPointer<PlusCtor>(plusAddr);
-
-IntPtr createNullableUnitAddr = Marshal.ReadIntPtr(symbols + step * createNullableUnitOffset);
-Void_IntPtr createNullableUnit = Marshal.GetDelegateForFunctionPointer<Void_IntPtr>(createNullableUnitAddr);
-
-IntPtr isInstanceAddr = Marshal.ReadIntPtr(symbols + step * isInstanceOffset);
-PtrPtr_Int isInstance = Marshal.GetDelegateForFunctionPointer<PtrPtr_Int>(isInstanceAddr);
-
-IntPtr disposeStablePointerAddr = Marshal.ReadIntPtr(symbols + step * disposeStablePointerOffset);
-Ptr_Void disposeStablePointer = Marshal.GetDelegateForFunctionPointer<Ptr_Void>(disposeStablePointerAddr);
+Void_IntPtr createNullableUnit = GetFunc<Void_IntPtr>(symbols, createNullableUnitOffset);
+PtrPtr_Int isInstance = GetFunc<PtrPtr_Int>(symbols, isInstanceOffset);
+Ptr_Void disposeStablePointer = GetFunc<Ptr_Void>(symbols, disposeStablePointerOffset);
 
 PrintHexed(symbols);
-PrintHexed(plusAddr);
-PrintHexed(createNullableUnitAddr);
-PrintHexed(disposeStablePointerAddr);
 
 IntPtr plus = plusCtor(2, 3);
 IntPtr plusTypeInst = plusType();
+IntPtr minus = minusCtor(2, 3);
 
-int x = isInstance(plus, plusTypeInst);
-Console.WriteLine(x);
-Console.WriteLine(plus);
+WriteLine(plus);
+WriteLine($"plus is plusType = {0 == isInstance(plus, plusTypeInst)}");
+WriteLine($"minus is plusType = {0 == isInstance(minus, plusTypeInst)}");
+
+WriteLine($"2 + 3 = {addMethod(plus)}");
+WriteLine($"2 - 3 = {subtractMethod(minus)}");
 
 IntPtr plusPinnedAddr = Marshal.ReadIntPtr(plus);
 disposeStablePointer(plusPinnedAddr);
+
 IntPtr unit = createNullableUnit();
-Console.WriteLine(unit);
+WriteLine(unit);
 
 
 delegate IntPtr SymbolsFunc();
 
 delegate IntPtr PlusCtor(int a, int b);
 
+delegate IntPtr MinusCtor(int a, int b);
+
 delegate IntPtr Void_IntPtr();
 
 delegate IntPtr Ptr_Void(IntPtr ptr);
 
 delegate int PtrPtr_Int(IntPtr inst, IntPtr type);
+
+delegate int Ptr_Int(IntPtr inst);
 
