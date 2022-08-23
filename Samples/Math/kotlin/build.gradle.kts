44a22fa88dcfc51a55ae928dd8cc1aeb0ace4a57
rico-erhard <76253376+rico-erhard@users.noreply.github.com>
2022-08-23 17:05:56 +0200

    Add a function to the kotlin lib that uses a callback.

--- Samples/Math/kotlin/build.gradle.kts
+++ Samples/Math/kotlin/build.gradle.kts
@@ -11,11 +11,16 @@ kotlin {
         // macosX64("native") { // on x86_64 macOS
         // macosArm64("native") { // on Apple Silicon macOS
         mingwX64("native") { // on Windows
+
+        val main by compilations.getting
+        val interop by main.cinterops.creating
+
         binaries {
             sharedLib {
                 baseName = "math"
             }
         }
+
     }
 }
 
--- Samples/Math/kotlin/src/nativeMain/kotlin/arithmetic/Plus.kt
+++ Samples/Math/kotlin/src/nativeMain/kotlin/arithmetic/Plus.kt
@@ -1,13 +1,20 @@
 package arithmetic
 
-class Plus (private val a: Int, private val b: Int) {
+import kotlinx.cinterop.*
+import interop.*
+
+class Plus(private val a: Int, private val b: Int) {
     fun add(): Int {
         return a + b
     }
 }
 
-class Minus (private val a: Int, private val b: Int) {
+class Minus(private val a: Int, private val b: Int) {
     fun subtract(): Int {
         return a - b
     }
+}
+
+class Callback() {
+    fun call(f: IntInt_Int, a: Int, b: Int): Int = f(a, b)
 }
