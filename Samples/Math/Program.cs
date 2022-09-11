b4ff6ecd7334a32d68622588327d4ebdd7e6ea2b
rico-erhard <76253376+rico-erhard@users.noreply.github.com>
2022-09-11 14:20:28 +0200

    Demonstrate injection of a function resolution and invocation in the sample with a function: double -> double -> double.

--- Samples/Math/Program.cs
+++ Samples/Math/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.InteropServices;
 using KotlinNative2Net;
 using static System.Console;
+using static System.Math;
 
 unsafe
 {
@@ -8,7 +9,7 @@ unsafe
     string apiPath = Path.GetDirectoryName(assemblyLocation) + Path.DirectorySeparatorChar + "math_api.h";
     string sharedLibPath = Path.GetDirectoryName(assemblyLocation) + Path.DirectorySeparatorChar + "math.dll";
 
-    using KLib kLib = (KLib)KLib.Of(apiPath, sharedLibPath);
+    using KLib kLib = (KLib)KLib.Of(apiPath, sharedLibPath, Invokers.Default.Add(new PtrDoubleDouble_DoubleInvoker()));
     dynamic dynKLib = kLib;
 
     dynamic dynPlus = dynKLib.root.arithmetic.Plus.Plus(2, 3);
@@ -24,9 +25,14 @@ unsafe
     int netAddResult = dynCallback.call<int>((IntPtr)netAdd, 2, 3);
     WriteLine($"2 + 3 = {netAddResult}");
 
+    dynamic dynFloatPlus = dynKLib.root.arithmetic.FloatPlus.FloatPlus();
+    double pi = dynFloatPlus.add(PI - 1, 1d);
+    WriteLine($"(π - 1) + 1 = {pi}");
+
     dynPlus.Dispose();
     dynMinus.Dispose();
     dynCallback.Dispose();
+    dynFloatPlus.Dispose();
     WriteLine("Bye.");
 
     [UnmanagedCallersOnly]
@@ -35,4 +41,21 @@ unsafe
         WriteLine($"Add {a} and {b} in .NET.");
         return a + b;
     }
+}
+
+
+delegate double PtrDoubleDouble_Double(IntPtr kObj, double a, double b);
+
+class PtrDoubleDouble_DoubleInvoker : Invoker
+{
+    public override object? Invoke(KLib kLib, KFunc func, IntPtr kObj, object?[] args)
+    => kLib
+        .GetFunc<PtrDoubleDouble_Double>(func)
+        .Map<double?>(d => d(kObj, (double)args[0], (double)args[1]))
+        .IfNoneUnsafe(() => null);
+
+    public override bool IsMatch(KFunc func, object[] args)
+    => 2 == args.Length
+        && func.Params.Skip(1).All(x => x.Type.EndsWith("KDouble"))
+        && func.RetVal.Type.EndsWith("KDouble");
 }
